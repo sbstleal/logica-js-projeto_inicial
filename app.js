cdf36e3075b94991f5480f31d06285d6842ba322
Sebastião Leal <sebastiaodeoleal@gmail.com>
2024-01-25 17:35:43 -0300

    Curso finalizado

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 let secretNumber;
 let guess;
+let maxRange = 50;
 
 function generateSecretNumber() {
-  // Gera um número aleatório entre 1 e 100
-  secretNumber = Math.floor(Math.random() * 100) + 1;
+  // Gera um número aleatório entre 1 e o valor determinado na variável maxRange
+  secretNumber = Math.floor(Math.random() * maxRange) + 1;
 }
 
 function playGame() {
   let attempts = 0;
   let guessedCorrectly = false;
+  let wordAttempt;
 
   while (!guessedCorrectly) {
-    guess = parseInt(prompt('Digite o seu palpite:'));
+    guess = parseInt(prompt(`Digite o seu palpite, um número entre 1 e ${maxRange}:`));
+    wordAttempt = attempts > 1 ? 'tentativas' : 'tentativa';   
     attempts++;
 
     if (guess === secretNumber) {
-      alert(`Parabéns! Você acertou o número secreto em ${attempts} tentativas.`);
+      alert(`Parabéns! Você acertou o número secreto em ${attempts} ${wordAttempt}.`);
       guessedCorrectly = true;
     } else if (guess < secretNumber) {
       alert('O número digitado é menor que o número secreto. Tente novamente.');
